d4495ce4a67e466795e3ca3b9df007c87359951c
Scott D. Miller <Scott_Miller@nps.gov>
2023-04-12 09:57:49 -0800

    Added DeploymentsForAFrequencyForm

---
+++ WRST_Caribou3/DeploymentsForAFrequencyForm.Designer.vb
@@ -0,0 +1,110 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class DeploymentsForAFrequencyForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ToolStrip1 = New System.Windows.Forms.ToolStrip()
+        Me.ToolStripLabel1 = New System.Windows.Forms.ToolStripLabel()
+        Me.FrequencyToolStripComboBox = New System.Windows.Forms.ToolStripComboBox()
+        Me.ToolStripLabel2 = New System.Windows.Forms.ToolStripLabel()
+        Me.ToleranceToolStripTextBox = New System.Windows.Forms.ToolStripTextBox()
+        Me.FrequenciesDataGridView = New System.Windows.Forms.DataGridView()
+        Me.ToolStripSeparator1 = New System.Windows.Forms.ToolStripSeparator()
+        Me.ToolStrip1.SuspendLayout()
+        CType(Me.FrequenciesDataGridView, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'ToolStrip1
+        '
+        Me.ToolStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripLabel1, Me.FrequencyToolStripComboBox, Me.ToolStripLabel2, Me.ToleranceToolStripTextBox, Me.ToolStripSeparator1})
+        Me.ToolStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.ToolStrip1.Name = "ToolStrip1"
+        Me.ToolStrip1.Size = New System.Drawing.Size(1064, 25)
+        Me.ToolStrip1.TabIndex = 0
+        Me.ToolStrip1.Text = "ToolStrip1"
+        '
+        'ToolStripLabel1
+        '
+        Me.ToolStripLabel1.Name = "ToolStripLabel1"
+        Me.ToolStripLabel1.Size = New System.Drawing.Size(154, 22)
+        Me.ToolStripLabel1.Text = "Deployments for frequency:"
+        '
+        'FrequencyToolStripComboBox
+        '
+        Me.FrequencyToolStripComboBox.Name = "FrequencyToolStripComboBox"
+        Me.FrequencyToolStripComboBox.Size = New System.Drawing.Size(121, 25)
+        '
+        'ToolStripLabel2
+        '
+        Me.ToolStripLabel2.Name = "ToolStripLabel2"
+        Me.ToolStripLabel2.Size = New System.Drawing.Size(25, 22)
+        Me.ToolStripLabel2.Text = "+/-"
+        '
+        'ToleranceToolStripTextBox
+        '
+        Me.ToleranceToolStripTextBox.Font = New System.Drawing.Font("Segoe UI", 9.0!)
+        Me.ToleranceToolStripTextBox.Name = "ToleranceToolStripTextBox"
+        Me.ToleranceToolStripTextBox.Size = New System.Drawing.Size(100, 25)
+        '
+        'FrequenciesDataGridView
+        '
+        Me.FrequenciesDataGridView.AllowUserToAddRows = False
+        Me.FrequenciesDataGridView.AllowUserToDeleteRows = False
+        Me.FrequenciesDataGridView.AllowUserToOrderColumns = True
+        Me.FrequenciesDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.FrequenciesDataGridView.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.FrequenciesDataGridView.Location = New System.Drawing.Point(0, 25)
+        Me.FrequenciesDataGridView.Name = "FrequenciesDataGridView"
+        Me.FrequenciesDataGridView.ReadOnly = True
+        Me.FrequenciesDataGridView.Size = New System.Drawing.Size(1064, 628)
+        Me.FrequenciesDataGridView.TabIndex = 1
+        '
+        'ToolStripSeparator1
+        '
+        Me.ToolStripSeparator1.Name = "ToolStripSeparator1"
+        Me.ToolStripSeparator1.Size = New System.Drawing.Size(6, 25)
+        '
+        'DeploymentsForAFrequencyForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1064, 653)
+        Me.Controls.Add(Me.FrequenciesDataGridView)
+        Me.Controls.Add(Me.ToolStrip1)
+        Me.Name = "DeploymentsForAFrequencyForm"
+        Me.Text = "DeploymentsForAFrequencyForm"
+        Me.ToolStrip1.ResumeLayout(False)
+        Me.ToolStrip1.PerformLayout()
+        CType(Me.FrequenciesDataGridView, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ToolStrip1 As ToolStrip
+    Friend WithEvents ToolStripLabel1 As ToolStripLabel
+    Friend WithEvents FrequencyToolStripComboBox As ToolStripComboBox
+    Friend WithEvents ToolStripLabel2 As ToolStripLabel
+    Friend WithEvents ToleranceToolStripTextBox As ToolStripTextBox
+    Friend WithEvents FrequenciesDataGridView As DataGridView
+    Friend WithEvents ToolStripSeparator1 As ToolStripSeparator
+End Class
---
+++ WRST_Caribou3/DeploymentsForAFrequencyForm.vb
@@ -0,0 +1,83 @@
+﻿Imports System.Data.SqlClient
+
+Public Class DeploymentsForAFrequencyForm
+
+    Public Sub New(Optional Frequency As Double = 0.000, Optional Tolerance As Double = 0.000)
+
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        Me.FrequencyToolStripComboBox.Text = Frequency
+        Me.ToleranceToolStripTextBox.Text = Tolerance
+
+        GetFrequencies()
+    End Sub
+
+    Private Sub GetFrequencies()
+        If IsNumeric(Me.FrequencyToolStripComboBox.Text) = True Then
+
+            Me.FrequenciesDataGridView.DataSource = Nothing
+
+
+            'This is the stored procedure and frequency to look for
+            Dim QueryString As String = "GetDeploymentsForAFrequency " & CDbl(Me.FrequencyToolStripComboBox.Text) & " " & IIf(IsNumeric(Me.ToleranceToolStripTextBox.Text) = True, ", " & Me.ToleranceToolStripTextBox.Text.Trim, ",0.000")
+
+            Try
+                Using Connection As New SqlConnection(My.Settings.WRST_CaribouConnectionString)
+                    Connection.Open()
+                    Dim Command As New SqlCommand(QueryString, Connection)
+                    Dim Reader As SqlDataReader = Command.ExecuteReader()
+                    Dim FrequenciesDataTable As New DataTable
+
+                    'Load the data table from the data reader
+                    FrequenciesDataTable.Load(Reader)
+
+                    Me.FrequenciesDataGridView.DataSource = FrequenciesDataTable
+
+                End Using
+            Catch ex As Exception
+            MessageBox.Show("Error while executing .. " & ex.Message, "")
+            End Try
+        Else
+            MsgBox("The collar frequency must be numeric. Canceled.", MsgBoxStyle.OkOnly)
+        End If
+    End Sub
+
+    Private Sub FrequencyToolStripComboBox_TextChanged(sender As Object, e As EventArgs) Handles FrequencyToolStripComboBox.TextChanged
+        GetFrequencies()
+    End Sub
+
+    Private Sub DeploymentsForAFrequencyForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        LoadAvailableFrequenciesIntoComboBox()
+    End Sub
+
+    Private Sub LoadAvailableFrequenciesIntoComboBox()
+        Try
+            Using Connection As New SqlConnection(My.Settings.WRST_CaribouConnectionString)
+                Connection.Open()
+                Dim Command As New SqlCommand("SELECT DISTINCT Frequency FROM tmpCollarDeployments ORDER BY Frequency", Connection)
+                Dim Reader As SqlDataReader = Command.ExecuteReader()
+                Dim FrequenciesDataTable As New DataTable
+
+                'Load the data table from the data reader
+                FrequenciesDataTable.Load(Reader)
+
+                For Each FrequencyRow As DataRow In FrequenciesDataTable.Rows
+                    Me.FrequencyToolStripComboBox.Items.Add(FrequencyRow.Item("Frequency"))
+                Next
+
+            End Using
+
+        Catch ex As Exception
+            MessageBox.Show("Error loading frequencies from database. " & ex.Message, "")
+        End Try
+    End Sub
+
+    Private Sub ToleranceToolStripTextBox_TextChanged(sender As Object, e As EventArgs) Handles ToleranceToolStripTextBox.TextChanged
+        If IsNumeric(Me.ToleranceToolStripTextBox.Text) = True Then
+            GetFrequencies()
+        Else
+            MsgBox("Tolerance must be numeric")
+        End If
+    End Sub
+End Class
